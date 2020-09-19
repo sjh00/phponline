518e9dcf3077ce97740540f9f21bb90f07871a57
sjh00 <sjh00@outlook.com>
2020-09-20 00:40:13 +0800

    直接运行

--- server/runCode.php
+++ server/runCode.php
@@ -5,7 +5,7 @@ if (isset($_POST['code'])) {
     //将代码中的`替换为空格，并进行URLCode解码
     $code = str_replace('`', ' ', urldecode($originalCode));
     //包含eval或sql或file或py
-    if (
+    /* if (
         strpos($code, 'eval') !== false ||
         strpos($code, 'sql') !== false ||
         strpos($code, 'file') !== false ||
@@ -13,7 +13,7 @@ if (isset($_POST['code'])) {
     ) {
         echo 'error';
         die;
-    }
+    } */
     //以< ? php或< ?开头
     if (substr($code, 0, 5) == '<?php') {
         $code = substr($code, 5);
@@ -27,5 +27,5 @@ if (isset($_POST['code'])) {
     //运行php代码
     $result = eval($code);
     //将经过URLCode编码的代码写入cookie，“/phponline/client”为前端代码所在的相对网站根目录的地址
-    setcookie('code', $originalCode, time() + 3600 * 24 * 7, '/phponline/client');
+    //setcookie('code', $originalCode, time() + 3600 * 24 * 7, '/phponline/client');
 }
